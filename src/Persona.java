92848ff6650e5b98ac9fc35af8d62758645f6676
MiquelAngelMontero <mmontero2@institutjaumehuguet.cat>
2022-01-19 09:56:24 +0100

    Persona1

--- src/Persona.java
+++ src/Persona.java
@@ -1,4 +1,22 @@
 
-public class Persona {
-
+public abstract class Persona {
+	
+	//Atributos
+	protected String nombre;
+	protected int edad;
+	protected char sexo;
+	
+	//Metodos
+	public boolean comprovarSexo() {
+		
+		if ((sexo == 'H') || (sexo == 'M')) {
+			return true;
+		}else {
+			return false;
+		}
+		
+	}
+	
+	public abstract boolean falta();
+	
 }
